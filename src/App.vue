09cd282478a22424006149097f58f9e682320253
Vincent Choy <vincent7q@gmail.com>
2026-01-03 09:08:40 +0800

    feat: create basic Vue.js application structure

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="godview-app">
+    <h1>GodViewActivation</h1>
+    <p>Overview Effect Journey - Coming Soon</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+onMounted(() => {
+  console.log('GodViewActivation initialized')
+})
+</script>
+
+<style scoped>
+#godview-app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to bottom, #000000, #0a0a1a);
+  color: #e0e0e0;
+}
+</style>
